5bf272a839931c01e038f167d9493baadb448f4a
digvijayy <digvijayy@gmail.com>
2019-01-06 14:51:47 -0500

    init and testing to be rsumed on mac. lambda does not play well with windows.

---
+++ index.js
@@ -0,0 +1,34 @@
+const chromium = require('chrome-aws-lambda');
+const puppeteer = require('puppeteer-core');
+
+exports.handler = async (event, context, callback) => {
+    let browser = null;
+
+    try {
+        browser = await puppeteer.launch({
+            args: chromium.args,
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath,
+            headless: chromium.headless,
+        });
+
+        let page = await browser.newPage();
+        if(event.url) {
+            await page.goto(event.url);
+            const result = event.options ? await page.pdf(event.options) : await page.pdf();
+            callback(null, result);
+        } else if(event.html) {
+            await page.setContent(event.html);
+            const result = event.options ? await page.pdf(event.options) : await page.pdf();
+            callback(null, result);
+        } else {
+            callback(Error("event.url or event.html is required"))
+        }
+    } catch (error) {
+        return callback(error);
+    } finally {
+        if (browser !== null) {
+            await browser.close();
+        }
+    }
+};
